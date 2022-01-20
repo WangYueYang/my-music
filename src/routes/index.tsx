d0d55c6584c5a72e373d3800f203e18e4e169291
wangyueyang <1632085751@qq.com>
2022-01-20 23:11:11 +0800

    feat: main padding bg

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
 import React, { lazy, Suspense } from "react";
 import { Routes, Route } from 'react-router-dom';
 
+import './index.css'
+
 const Home = lazy(() => import('@page/home'))
 
 const RouteFc = () => {
 
   return (
     <Suspense fallback={<div>loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </Suspense>
   )
 }
