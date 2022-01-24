9b6b426c0df900c28fe9e0fce43aeecfb6c8f372
wangyueyang <1632085751@qq.com>
2022-01-24 20:36:45 +0800

    chore: 改用 axios 。。。

--- src/api/api-util.ts
+++ src/api/api-util.ts
@@ -1,4 +1,6 @@
-export type methodType = 'POST' | 'GET';
+import axios from 'axios';
+
+export type methodType = 'get' | 'post';
 
 export type paramsType = unknown;
 
@@ -8,17 +10,39 @@ export interface Request {
   params?: paramsType;
 }
 
-const request = ({ method, url, params }: Request): Promise<unknown> => {
+const server = axios.create({
+  timeout: 10000,
+});
+
+server.interceptors.request.use((options) => {
+  return options;
+});
+
+server.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+    return res;
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+const request = ({ method, url, params = {} }: Request): Promise<unknown> => {
+  let data = {};
+  // get请求使用params字段
+  if (method == 'get') data = { params };
+  // post请求使用data字段
+  if (method == 'post') data = { data: params };
   return new Promise((resolve, reject) => {
-    fetch(url, {
+    server({
+      url,
       method,
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(params),
+      ...data,
     })
-      .then((res) => res.json())
-      .then((res) => resolve(res))
+      .then((res) => {
+        resolve(res);
+      })
       .catch((err) => {
         throw new Error(err);
       });
@@ -26,9 +50,10 @@ const request = ({ method, url, params }: Request): Promise<unknown> => {
 };
 
 export const get = (url: string, params?: paramsType) => {
-  return request({ method: 'GET', url, params });
+  return request({ method: 'get', url, params });
 };
 
 export const post = (url: string, params?: paramsType) => {
-  return request({ method: 'GET', url, params });
+  return request({ method: 'post', url, params });
+  // return axios.post(url, params)
 };
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,5 @@ import { post, get, paramsType } from './api-util';
 
 // /artist/list
 export const apiArtistList = (): Promise<unknown> => {
-  return get('/api/artist/list')
+  return post('/api/artist/list')
 };
