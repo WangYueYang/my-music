8a06e7cb49ac61d198224de7ea6c72503ca7b48d
wangyueyang <1632085751@qq.com>
2022-02-01 20:53:34 +0800

    fix: DailyPush 背景图 src

--- src/components/DailyPush/index.tsx
+++ src/components/DailyPush/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import PlayButton from '@components/PlayButton'; 
-import CoverImg from '@assets/img/daily-push-bg.jpeg'
+import PlayButton from '@components/PlayButton';
+import CoverImg from '@assets/img/daily-push-bg.jpeg';
 import './index.css';
 
 const DailyPush = (): JSX.Element => {
+  const coverImg = CoverImg as string;
+
   return (
     <div className="daily-push">
-      <img src={CoverImg.src} alt="" />
+      <img src={coverImg} alt="" />
       <div className="content">
         <div className="text-box">
           <p className="text">每日推荐</p>
