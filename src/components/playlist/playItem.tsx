d6487c04a63edb639d4beaaa4eb6c9d73a94fa5b
wangyueyang <1632085751@qq.com>
2022-03-02 21:37:49 +0800

    style(playItem): 添加 playBtn 和背景模糊

--- src/components/playlist/playItem.tsx
+++ src/components/playlist/playItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import PlayButton from '../PlayButton';
 import { Link } from 'react-router-dom';
 
 import './PlayItem.css';
@@ -11,11 +12,18 @@ type PlayItemProps = {
   className?: string;
 };
 
-const PlayItem = ({ picUrl, name, copywriter, className }: PlayItemProps): JSX.Element => {
+const PlayItem = ({
+  picUrl,
+  name,
+  copywriter,
+  className,
+}: PlayItemProps): JSX.Element => {
   return (
     <div className={classNames(className, 'play-item')}>
       <div className="item-img">
+        <PlayButton />
         <img src={picUrl} alt="" />
+        <img src={picUrl} alt="" className="bg" />
       </div>
       <p className="item-title">
         <Link to="/">{name}</Link>
