16ce231a50df772f095a850f1f702912ef6a81aa
wangyueyang <1632085751@qq.com>
2022-03-05 22:41:01 +0800

    chore(util): add getCookie removeCookie

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,24 @@
-export function setCookie(cookie: string) {
+import Cookies from 'js-cookie';
+
+export function setCookie(cookie: string): void {
   const cookies = cookie.split(';;');
   cookies.map((cookie: string) => {
     document.cookie = cookie;
     const cookieKeyValue = cookie.split(';')[0].split('=');
     localStorage.setItem(`cookie-${cookieKeyValue[0]}`, cookieKeyValue[1]);
   });
+}
+
+export function getCookie(key: string): string | null {
+  return Cookies.get(key) ?? localStorage.getItem(`cookie-${key}`);
+}
+
+// MUSIC_U 只在登录时才有
+export function isLogin(): boolean {
+  return getCookie('MUSIC_U') !== undefined;
+}
+
+export function removeCookie(key: string) {
+  Cookies.remove(key);
+  localStorage.removeItem(`cookie-${key}`);
 }
