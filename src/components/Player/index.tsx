55e4eca7698c512c69107414d9dadebeb99d8fc2
wangyueyang <1632085751@qq.com>
2022-04-10 21:59:56 +0800

    feat: audio 播放 url 音乐

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { playSongUrl } from '@store/play';
 
 const Player = (): JSX.Element => {
-  
+  const songUrl = useRecoilValue(playSongUrl);
+  const url = songUrl[0]?.url || '';
+
+
   return (
     <div className="player">
-      player
-      <audio src=""></audio>
+      <audio src={url} autoPlay>
+        哈哈哈播放不了气不气？
+      </audio>
     </div>
   );
 };
